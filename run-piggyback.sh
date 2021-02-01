acc06ca9996a70b1718ff83bde446cc89364ffa0
GioF_71 <giovanni.fulco@gmail.com>
2021-02-01 14:05:06 +0100

    own vs regular

--- run-piggyback.sh
+++ run-piggyback.sh
@@ -4,4 +4,8 @@ echo "About to sleep for $STARTUP_DELAY_SEC second(s)"
 sleep $STARTUP_DELAY_SEC
 echo "Rise and shine!"
 
-/start.sh $@
+#/start.sh $@
+
+#-a aune-s6-airplay -- -d hw:DAC -c PCM
+
+/start.sh -a $SHAIRPORT_SYNC_NAME -- -d $SHAIRPORT_SYNC_AUDIO_DEVICE -m $SHAIRPORT_SYNC_MIXER
