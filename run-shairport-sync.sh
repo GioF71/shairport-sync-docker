97d0cdf020e72d546df125c3275927a387dee65c
GioF_71 <giovanni.fulco@gmail.com>
2021-02-06 00:04:57 +0100

    remove avahi

--- run-shairport-sync.sh
+++ run-shairport-sync.sh
@@ -4,17 +4,17 @@ echo "About to sleep for $STARTUP_DELAY_SEC second(s)"
 sleep $STARTUP_DELAY_SEC
 echo "Rise and shine!"
 
-echo "starting dbus-uuidgen ..."
-dbus-uuidgen --ensure
+#echo "starting dbus-uuidgen ..."
+#dbus-uuidgen --ensure
 
-cat /var/lib/dbus/machine-id
+#cat /var/lib/dbus/machine-id
 
-echo "starting dbus-daemon ..."
-mkdir -p /var/run/dbus
-dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+#echo "starting dbus-daemon ..."
+#mkdir -p /var/run/dbus
+#dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
 
-echo "starting avahi-daemon ..."
-avahi-daemon --daemonize --no-chroot
+#echo "starting avahi-daemon ..."
+#avahi-daemon --daemonize --no-chroot
 
 echo "starting shairport-sync ..."
 #shairport-sync -a $SHAIRPORT_SYNC_NAME -- -d $SHAIRPORT_SYNC_AUDIO_DEVICE -m $SHAIRPORT_SYNC_MIXER -c PCM
