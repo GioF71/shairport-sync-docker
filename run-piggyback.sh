766fcaac321a974cfe472ac1c9d639e37bc67908
GioF_71 <giovanni.fulco@gmail.com>
2021-02-12 17:19:02 +0100

    removed mixer

--- run-piggyback.sh
+++ run-piggyback.sh
@@ -4,4 +4,4 @@ echo "About to sleep for $STARTUP_DELAY_SEC second(s)"
 sleep $STARTUP_DELAY_SEC
 echo "Rise and shine!"
 
-/start.sh -a $SHAIRPORT_SYNC_NAME -- -d $SHAIRPORT_SYNC_AUDIO_DEVICE -m $SHAIRPORT_SYNC_MIXER
+/start.sh -a $SHAIRPORT_SYNC_NAME -- -d $SHAIRPORT_SYNC_AUDIO_DEVICE
